7f722fe574c77550c5e0fbf8fa243ee8c36077a7
Cullen Jett <cullenjett@gmail.com>
2015-06-15 16:38:37 -0600

    nothing

--- spec/quickbase_record_config.rb
+++ spec/quickbase_record_config.rb
@@ -1,7 +1,5 @@
 QuickbaseRecord.configure do |config|
   config.realm = "ais"
-  config.username = "cullenjett"
-  # config.username = ENV["QB_USERNAME"]
-  config.password = "condor88"
-  # config.password = ENV["QB_PASSWORD"]
+  config.username = ENV["QB_USERNAME"]
+  config.password = ENV["QB_PASSWORD"]
 end
