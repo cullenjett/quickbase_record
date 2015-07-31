dd29d84f18c443c971ff0451c74242debb497416
Cullen Jett <cullenjett@gmail.com>
2015-07-31 09:42:50 -0600

    nothing important...

--- spec/quickbase_record_config.rb
+++ spec/quickbase_record_config.rb
@@ -1,5 +1,8 @@
+require 'yaml'
+creds = YAML.load_file("spec/config.yml")
+
 QuickbaseRecord.configure do |config|
   config.realm = "ais"
-  config.username = "cullenjett"
-  config.password = "condor88"
+  config.username = creds["username"]
+  config.password = creds["password"]
 end
