d7f1b7506d7fc00a563416edeb15b2cbd66ee587
Kit Hensel <kithensel@gmail.com>
2016-11-17 14:59:54 -0800

    added usertoken auth

--- lib/quickbase_record/configuration.rb
+++ lib/quickbase_record/configuration.rb
@@ -1,12 +1,13 @@
 module QuickbaseRecord
   class Configuration
-    attr_accessor :realm, :username, :password, :token
+    attr_accessor :realm, :username, :password, :token, :usertoken
 
     def initialize
       @realm = ''
       @username = ''
       @password = ''
       @token = ''
+      @usertoken = ''
     end
   end
 end
